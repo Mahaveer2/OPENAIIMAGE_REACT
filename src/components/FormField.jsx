f575f70073b8a48d9c1b26d9d16b9c00b3e12b16
Partab Rai <amrani.partab@gmail.com>
2023-01-28 18:11:25 +0500

    initial

---
+++ src/components/FormField.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FormField = ({labelName,type,name,placeholder,value,handleChange,isSupriseMe,handleSupriseMe}) => {
+  return (
+    <div>
+
+      <div className='flex items-center gap-2 mb-2'>
+        <label htmlFor={name} className='block text-sm font-medium text-gray-900'>{labelName}</label>
+        {isSupriseMe && (<button type='button' onClick={handleSupriseMe} className='font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black'>Suprise me</button>) }
+      </div>
+      <input type={type} id={name} name={name} placeholder={placeholder} value={value} onChange={handleChange} require className='bg-gray-50 border-gray-300 border text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3' />
+    </div>
+  )
+}
+
+export default FormField
